04ae315e3f3adda24b0d000f1f879475b44e2047
JmrDn <ptc.jomaridanao@gmail.com>
2024-04-21 02:43:01 +0800

    Fixed scroll selected properties, hover of ourproperties in landpage

--- Assets/JS/news-and-events.js
+++ Assets/JS/news-and-events.js
@@ -1,3 +1,3 @@
 document.querySelector('.item8').addEventListener('click', function(){
-    window.location.href ="../../Modules/News and Events/Contents/Sale/martin-ville-open-house.html";
+    window.location.href ="../../Modules/News and Events/Contents/martin-ville-open-house.html";
 });
