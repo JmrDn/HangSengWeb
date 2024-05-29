1b346288379ccff34f5cb9e68268da3c196a035e
JmrDn <ptc.jomaridanao@gmail.com>
2024-05-29 16:15:53 +0800

    Responsiveness and Function of for sale overview
    resposiveness of:
    For sale overview
    Our Team

---
+++ Assets/JS/for-sale-overview.js
@@ -0,0 +1,8 @@
+document.getElementById('fracer-place').addEventListener('click', function(){
+    window.location.href ="/Modules/Our properties/For Sale/fraser-place.html";
+});
+
+document.getElementById('anvaya-cove').addEventListener('click', function(){
+    window.location.href ="/Modules/Our properties/For Sale/anvaya-cove.html";
+});
+
